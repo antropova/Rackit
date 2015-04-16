bb1788839917a20fb15fdb830d40c5b6f8f17aa2
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-04-16 10:38:57 -0400

    Should work with markers once dan pushes his changes

---
+++ app/controllers/corrals_controller.rb
@@ -0,0 +1,9 @@
+class CorralsController < ApplicationController
+  def index
+    @users = User.all
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+    end
+  end
+end
