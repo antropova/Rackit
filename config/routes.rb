2d0f4e50a7653d016aba501ecff4cee6ec4883d8
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-04-15 22:49:21 -0400

    Add sign out link and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   root 'welcome#index'
   get 'auth/:provider/callback', to: 'sessions#create'
+  get 'signout', to: 'sessions#destroy', as: :signout
 
 
 
