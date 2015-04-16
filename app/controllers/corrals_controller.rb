e6c25f8d199f18e081f22f40608d7a023f794266
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-04-16 10:45:44 -0400

    Change users to corrals

--- app/controllers/corrals_controller.rb
+++ app/controllers/corrals_controller.rb
@@ -1,9 +1,9 @@
 class CorralsController < ApplicationController
   def index
-    @users = User.all
-    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
-      marker.lat user.latitude
-      marker.lng user.longitude
+    @corrals = Corral.all
+    @hash = Gmaps4rails.build_markers(@corrals) do |corral, marker|
+      marker.lat corral.latitude
+      marker.lng corral.longitude
     end
   end
 end
