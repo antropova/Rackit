de6772ce03e79e59a3ed0f4d346bcd4bc9a46271
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-04-15 22:16:51 -0400

    Add twitter omniauth, sessions, routes and user pages and controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Welcome back, #{user.name}!"
+      redirect_to root_url
+    else
+      @user = User.create_with_omniauth(auth)
+      render '/users/new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:danger] = "Signed Out!"
+    redirect_to root_url
+  end
+
+  private
+  def auth
+    request.env["omniauth.auth"]
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,84 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :change_user, only: [:edit, :update, :destroy]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    # @user = User.find(current_user.id)
+  end
+
+  def edit
+  end
+
+  # GET /users/new
+  def new
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:success] = 'Your profile was created successfully!'
+        format.html { redirect_to home_url }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      # binding.pry
+      if @user.update(user_params)
+        flash[:success] = "Your profile was successfully updated. "
+        format.html { redirect_to @user}
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, success: 'Your account has been successfully deleted.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def change_user
+    if current_user != set_user
+      flash[:danger] = "Sorry, you cant #{params[:action]} this user. Click this message to close it"
+      redirect_to set_user
+    end
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:id, :name, :provider, :uid)
+  end
+end
