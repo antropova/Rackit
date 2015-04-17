cfd05795086fd27d3dfbe3c7d0b004ea4b87f520
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-04-17 08:39:48 -0400

    Continue work on google maps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415135338) do
+ActiveRecord::Schema.define(version: 20150416134520) do
 
   create_table "bike_corrals", force: :cascade do |t|
     t.integer  "bike_id"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20150415135338) do
     t.integer  "borough_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "corrals", ["borough_id"], name: "index_corrals_on_borough_id"
