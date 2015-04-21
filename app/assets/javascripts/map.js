9528ebe6b875130208705299b58cccce399b39d1
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-04-21 08:49:52 -0400

    Fix merge conflicts in map.js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,23 +1,4 @@
 $( document ).ready(function() {
-<<<<<<< HEAD
-  handler = Gmaps.build('Google');
-  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-    markers = handler.addMarkers([
-    {
-    
-      "picture": {
-        "url": "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
-        "width":  36,
-        "height": 36
-      },
-      "infowindow": "hello!"
-    }
-  ]);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
-  });
-=======
-
   if ($("#map").length >= 1 ) {
     handler = Gmaps.build('Google');
     handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
@@ -26,5 +7,4 @@ $( document ).ready(function() {
       handler.fitMapToBounds();
     });
   }
->>>>>>> fdc9f16ec159b30298489150ed4ff43e7f9d000d
 });
