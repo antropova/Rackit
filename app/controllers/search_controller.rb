e67ffe8e40eb62db71142ab59bad62178481cfb9
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-04-22 16:40:48 -0400

    Improve search suggestion results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 class SearchController < ApplicationController
   def index
     @corrals = Corral.search(params[:search])
+    if @corrals.size == 1 
+      redirect_to corral_path(@corrals.first)
+    end
   end
 
   def autocomplete
