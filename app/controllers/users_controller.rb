0c5671302f2aa24c49c7f54ea14a7cd46a7ba734
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-04-22 13:52:33 -0400

    Delete one line

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
       marker.lat(corral.latitude)
       marker.lng(corral.longitude)
       marker.infowindow corral.location
-      iconBase = 'https://maps.google.com/mapfiles/kml/shapes/'
       marker.picture({
         "url" => "http://www.edmonton.ca/activities_parks_recreation/documents/Logos/icon_bike_32x32.png",
         "width" => 35,
