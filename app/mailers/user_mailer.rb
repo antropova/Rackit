221b3da0f3671abb7d28f21a11481eda80031484
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-04-23 18:15:26 -0400

    Test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,8 @@
 class UserMailer < ApplicationMailer
 
-  def registration_confirmation(user)
-    @user = user
-    # attachments["bike_icon.png"] = File.read("")
-    # binding.pry
-    mail to: @user.email
-    # mail(to: 'mikel@test.lindsaar.net') do |format|
-    #   format.text { render plain: "Hello Mikel!" }
-    #   format.html { render html: "<h1>Hello Mikel!</h1>".html_safe }
-    # end
-  end
+ def registration_confirmation(user)
+   @user = user
+   # attachments["bike_icon.png"] = File.read("")
+   mail(to: @user.email, subject: "Registered")
+ end
 end
