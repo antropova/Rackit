392e9a65d998252ab767a5234bf6dbb0c72bdcc2
DanFerrer <danielferrer212@gmail.com>
2015-04-24 14:59:08 -0400

    fixed attractions:

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,5 +1,5 @@
 class AttractionsController < ApplicationController
   def index
-    @spots = client.spots(user_location["latitude"], user_location["longitude"], types: ['restaurant', 'food', 'cafe', 'museum', 'park', 'bakery'], radius: 800)
+    @spots = client.spots(current_user.current_latitude, current_user.current_longitude, types: ['restaurant', 'food', 'cafe', 'museum', 'park', 'bakery'], radius: 800)
   end
 end
