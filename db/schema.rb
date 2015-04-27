fd972f9195f120d632876361f5695b630207fb91
DanFerrer <danielferrer212@gmail.com>
2015-04-27 14:48:59 -0400

    fixed merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427164649) do
+ActiveRecord::Schema.define(version: 20150427180623) do
+
   create_table "bikes", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at", null: false
