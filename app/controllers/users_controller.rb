82ac34af23f2963b63c0da3b758c8bf6fa20f2c2
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-04-29 13:02:17 -0400

    Add links to infowindows of corrals

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     @hash_two = Gmaps4rails.build_markers(@corrals) do |corral, marker|
       marker.lat(corral.latitude)
       marker.lng(corral.longitude)
-      marker.infowindow corral.location
+      marker.infowindow render_to_string(:partial => "/corrals/my_template", :locals => { :object => corral })
       marker.picture({
         "url" => "http://www.edmonton.ca/activities_parks_recreation/documents/Logos/icon_bike_32x32.png",
         "width" => 50,
