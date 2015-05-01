1f4e10f3a204db41c5c0f34052ef3c3c7cd8b848
Your Name <you@example.com>
2015-05-01 09:15:39 -0400

    deleted repeated piece of code

--- app/models/corral.rb
+++ app/models/corral.rb
@@ -3,13 +3,12 @@ class Corral < ActiveRecord::Base
   has_many :users, through: :checkins
   has_many :bikes, through: :bike_corrals
   has_many :checkins
-<<<<<<< HEAD
-  has_many :checkouts
+
   has_many :users, through: :checkins
-=======
+
   has_many :reviews, through: :checkins
   has_many :reviewers, -> { uniq }, through: :reviews, source: :user
->>>>>>> 0f267f6e82433d6dd13b5757b12103d780eea927
+
   belongs_to :borough
   geocoded_by :location
   after_validation :geocode, if: ->(obj) { !obj.location.present? }
